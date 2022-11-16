46815cfd6326898b5830244db228ed3d23ec9c3a
Juniooor <juniormodern@hotmail.com>
2022-11-16 20:18:58 -0300

    api função criar, ok

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from flask import Flask, make_response, jsonify, request
 from bd import Carros
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/carros', methods=['GET'])
 def get_carros():
     return make_response(jsonify(Carros) ) 
 
-@app.route('/')
+@app.route('/carros', methods=['POST'])
 def create_car():
     carro = request.json
     Carros.append(carro)
-    return carro
+    return make_response(jsonify(Carros))
 
 app.run()
